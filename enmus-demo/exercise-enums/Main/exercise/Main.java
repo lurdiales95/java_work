09659cbdb43953a39112c50ab84cecbcf4d50cd4
lurdiales95 <lizethurdiales@gmail.com>
2025-05-29 14:31:12 -0500

    Sports Statistics Section Completed

---
+++ enmus-demo/exercise-enums/Main/exercise/Main.java
@@ -0,0 +1,18 @@
+package Main.exercise;
+
+public class Main {
+    enum CoffeeSize {
+            SMALL,
+            MEDIUM,
+            LARGE,
+
+
+
+    }
+    public static void main(CoffeeSize[] args) {
+    CoffeeSize[] Selected = CoffeeSize.values();
+            System.out.println(CoffeeSize[1]);
+
+
+    }
+}
--- enmus-demo/src/Main.java
+++ enmus-demo/src/Main.java
@@ -1,15 +1,32 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    enum OrderStatus {
+        ORDERED,
+        WAREHOUSE,
+        SHIPPED,
+        DELIVERED,
+        RETURNED,
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+    enum TrafficLight {
+        RED,
+        YELLOW,
+        GREEN,
+    }
+
+    enum  GamesStatus {
+        WIN,
+        LOSS,
+        TIE
+    }
+
+    public static void main(String[] args) {
+
+        TrafficLight[] indexes = TrafficLight.values();
+
+        TrafficLight light = indexes[2];
+
+        System.out.println(light);
     }
 }
