4a8783802167fcda67dcda976eb78cf0488831b1
lurdiales95 <lizethurdiales@gmail.com>
2025-05-30 14:57:31 -0500

    emmus demo sample finished

--- enmus-demo/src/Main.java
+++ enmus-demo/src/Main.java
@@ -1,32 +1,49 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     enum OrderStatus {
+
         ORDERED,
         WAREHOUSE,
         SHIPPED,
         DELIVERED,
-        RETURNED,
+        RETURNED
     }
 
     enum TrafficLight {
         RED,
         YELLOW,
-        GREEN,
+        GREEN
     }
 
-    enum  GamesStatus {
-        WIN,
+    enum GameStatus {
+        TIE,
         LOSS,
-        TIE
+        WIN
     }
+
 
     public static void main(String[] args) {
+        //order status, 1 ordered, 2 warehouse, 3 shipped , 4 delivered, 5 returned
+
+        OrderStatus status = OrderStatus.WAREHOUSE;
+        // do stuff for warehouse processing
+        System.out.println(OrderStatus.DELIVERED);
+        System.out.println(OrderStatus.DELIVERED.ordinal());
+
+        // if (status == OrderStatus.WAREHOUSE) {
+            //do stuff for warehouse processing
+            // }
+
+        GameStatus[] statuses = GameStatus.values();
+        GameStatus tie = statuses[0];
+
+        System.out.println(tie);
 
         TrafficLight[] indexes = TrafficLight.values();
 
         TrafficLight light = indexes[2];
 
         System.out.println(light);
+
+
     }
 }
