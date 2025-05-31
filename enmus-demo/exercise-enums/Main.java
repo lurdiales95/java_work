095ce935fb4993e84b75618aed268aa213297b37
lurdiales95 <lizethurdiales@gmail.com>
2025-05-30 19:15:22 -0500

    Deleted duplicate files

---
+++ enmus-demo/exercise-enums/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+enum CoffeeSize{
+    SMALL,
+    MEDIUM,
+    LARGE,
+}
+
+    }
+    public static void main(String[] args) {
+
+        CoffeeSize status =  CoffeeSize.MEDIUM;
+
+        System.out.println("Selected coffee size:" + CoffeeSize.MEDIUM);
+
+    }
+}
