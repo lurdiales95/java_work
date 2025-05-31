566330635623dc937c36693f419be217b6ca45f9
lurdiales95 <lizethurdiales@gmail.com>
2025-05-31 18:33:41 -0500

    Shopping Cart pt. 1 Complete

--- shopping_cart_exercise/src/Main.java
+++ shopping_cart_exercise/src/Main.java
@@ -1,15 +1,24 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args) {
+
+        //Declaring and assigning num variables
+        String productID = "1";
+        String productCategory = "2";
+        double productCost = 2.56;
+        double  productPrice = 4.99;
+        int productQuantity = 78;
+
+        //calcuating
+        double profitMargin =  (productPrice * productQuantity);
+        System.out.println("Profit Margin: $" + profitMargin);
+
+        double totalCost = (productCost * productQuantity);
+        System.out.println("Total Cost: $" + totalCost);
+
+        double potentialProfit = (profitMargin - totalCost);
+        System.out.printf("Potential Profit: $%.2f\n", potentialProfit);
+
+
         }
     }
-}
