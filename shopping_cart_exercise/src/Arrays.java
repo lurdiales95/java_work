136653d8754bfa28372c88292b81b9dc8d78c451
lurdiales95 <lizethurdiales@gmail.com>
2025-06-02 14:12:09 -0500

    Arrays exercise complete

---
+++ shopping_cart_exercise/src/Arrays.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Arrays {
+    public static void main(String[] args) {
+        //shipping
+        String[] addresses = {"123 Main St", "456 Main St", "789 Main St"};
+
+
+
+
+
+
+
+
+
+        }
+    }String[] sizes = {"small", "medium", "large"};
+}
