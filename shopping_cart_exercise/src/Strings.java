9ff0157b19c6270cd7de9ac6da13f63068bb33a6
lurdiales95 <lizethurdiales@gmail.com>
2025-06-01 22:33:11 -0500

    Strings Shopping Cart Exercise Complete
    Finished Strings Exercise

---
+++ shopping_cart_exercise/src/Strings.java
@@ -0,0 +1,12 @@
+public class Strings {
+
+    public static void main(String[] args) {
+        String businessName = "Superstore";
+        String contactInfo = "Contact us at: 555-555-5555";
+        String itemDescription =  "Fresh organic apples - 1 lb";
+
+        System.out.println(businessName);
+        System.out.println(contactInfo);
+        System.out.println(itemDescription);
+    }
+}
