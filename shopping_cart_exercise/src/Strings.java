735095057f17231fa1b1989ad1b06298ec232eff
lurdiales95 <lizethurdiales@gmail.com>
2025-06-01 23:26:52 -0500

    Adding shopping cart strings tab

--- shopping_cart_exercise/src/Strings.java
+++ shopping_cart_exercise/src/Strings.java
@@ -3,10 +3,10 @@ public class Strings {
     public static void main(String[] args) {
         String businessName = "Superstore";
         String contactInfo = "Contact us at: 555-555-5555";
-        String itemDescription =  "Fresh organic apples - 1 lb";
+        String productDescription =  "Sunblock SPF 70";
 
         System.out.println(businessName);
         System.out.println(contactInfo);
-        System.out.println(itemDescription);
+        System.out.println(productDescription);
     }
 }
