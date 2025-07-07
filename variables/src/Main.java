48ef63cd10cebc69a391b35336b1f838b4aebf9d
lurdiales95 <lizethurdiales@gmail.com>
2025-07-06 20:07:44 -0500

    ArraysNotes going back to the beginning

---
+++ variables/src/Main.java
@@ -0,0 +1,82 @@
+public class Main {
+    public static void main(String[] args) {
+//        int num = 42; // when assigning numbers, use an equal sign =
+//        num = 57;
+//
+//        System.out.println(num);
+//
+//        int count1, count2, count3;
+//
+//        count1 = 42;
+//        count2 = 13;
+//        count3 = 5;
+//
+//        System.out.println(count1);
+//        System.out.println(count2);
+//        System.out.println(count3);
+//
+//        // you have to declare what some is, in this case, it's an integer
+//        int sum = count1 + count2;
+//        // if you print, it'll tell you that it cannot print because count4 hasn't been declared/initialized
+//        System.out.println(count4);
+//
+//
+//         // primitive data types
+//        boolean val1 = true;
+//
+//        // single character
+//        char c= 'a';
+//
+//        // numercis
+//        // whole numbers
+//        byte b;  //-128 to 127
+//        short s; // -32,768 to 32,767
+//        int i; // -2B to +2B
+//        long l = 9_000_000; // huge number
+//
+//        //floating point (decimals)
+//        float pi = 3.145926f; // f has to be used to make sure you declare so that it knows it's a float number. 7 decimal places of precision
+//        double d = 3.14; // f will go up to 15 decimal places of persuasion.
+
+//        int x = 0;
+//
+//        x = 1 + 2;
+//        x = 4 - 2;
+//        x = 4 * 3;
+//        x = 12 / 4;
+//
+//        x += 3;
+//        x -= 3;
+//        x *= 2;
+//        x /= 4;
+//
+//        x++; // add 1
+//        x--; // minus 1
+//        x = x +1;
+//
+
+//       //  mixed int and float
+//        int x = 13;
+//        float y = 2.0f;
+//
+//        System.out.println( x / 2); // will give you a 6 because it will truncate remainders. Integer is whole number
+//
+//        System.out.println(x / y); /* decimal given b/c float will be more precise. Goes wiht precision for answers.
+//
+ //       Showing boolean with equals for true ro false
+//        int x = 13;
+//        int y = 5;
+//
+//        System.out.println(x == y); // will return true or false. this will be false.
+//        System.out.println(x != y); // will return true b/c x does not equal y
+//
+        int x = 5;
+        int y = 5;
+
+        boolean b1 = true;
+        boolean b2 = !b1; // not
+
+        System.out.println(x != y);
+
+        }
+    }
