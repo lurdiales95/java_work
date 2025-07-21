23050c6c24ab4870c80fefe2df0026ce0e41befc
lurdiales95 <lizethurdiales@gmail.com>
2025-07-21 02:26:43 -0500

    Updates to app

---
+++ Capstone-Java-Console-App-Inventory-Manager/src/main/java/com/example/Capstone_Java_Console_App_Inventory_Manager/CapstoneJavaConsoleAppInventoryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.Capstone_Java_Console_App_Inventory_Manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneJavaConsoleAppInventoryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneJavaConsoleAppInventoryManagerApplication.class, args);
+	}
+
+}
---
+++ Capstone-Java-Console-App-Inventory-Manager/src/test/java/com/example/Capstone_Java_Console_App_Inventory_Manager/CapstoneJavaConsoleAppInventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Capstone_Java_Console_App_Inventory_Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneJavaConsoleAppInventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
