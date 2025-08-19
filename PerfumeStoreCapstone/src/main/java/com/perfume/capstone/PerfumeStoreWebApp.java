006e3662fdadf4a3beeaca66af6b7791140894ba
lurdiales95 <lizethurdiales@gmail.com>
2025-08-19 16:35:10 -0500

    Capstone progress

--- PerfumeStoreCapstone/src/main/java/com/perfume/capstone/PerfumeStoreWebApp.java
+++ PerfumeStoreCapstone/src/main/java/com/perfume/capstone/PerfumeStoreWebApp.java
@@ -10,6 +10,4 @@ public class PerfumeStoreWebApp {  // Renamed for clarity
         SpringApplication.run(PerfumeStoreWebApp.class, args);
     }
 
-    // That's it! No CommandLineRunner, no console dependencies
-    // Spring Boot will start the web server automatically
 }
